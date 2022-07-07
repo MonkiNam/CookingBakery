72a200bac026a8be5eb14cf24617e5ab1f3bc1e1
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-07 14:43:39 +0700

    Add field ImageUrl to Event

--- EventManagementFPT/Model/Event.cs
+++ EventManagementFPT/Model/Event.cs
@@ -20,6 +20,7 @@ namespace EventManagementFPT.Model
         public Guid EventId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
