04eb20f0120b5478c54a0708fb279d1d0ba4a1b4
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-07 18:55:36 +0700

    Seed data

--- EventManagementFPT/Program.cs
+++ EventManagementFPT/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using EventManagementFPT.Model;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EventManagementFPT
 {
@@ -7,7 +11,25 @@ namespace EventManagementFPT
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<EventManagementContext>();
+                    Seed.SeedData(context).Wait();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured during migration");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
