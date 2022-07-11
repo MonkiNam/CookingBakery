d2b48456fd31d5f3f48339027e8151f8f3f8d531
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-11 17:55:12 +0700

    Update UserEvent.cs

--- EventManagementFPT/Model/UserEvent.cs
+++ EventManagementFPT/Model/UserEvent.cs
@@ -9,7 +9,13 @@ namespace EventManagementFPT.Model
     public partial class UserEvent
     {
         public Guid UserId { get; set; }
+
         public Guid EventId { get; set; }
+
+        public bool IsHost { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
         [NotMapped] 
         public int Going { get; set; }
 
