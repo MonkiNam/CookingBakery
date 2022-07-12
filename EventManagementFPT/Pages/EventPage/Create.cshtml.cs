b884dc777c0851749596faa1ad45d67d7a2ad208
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-12 14:01:56 +0700

    Authorize create event

--- EventManagementFPT/Pages/EventPage/Create.cshtml.cs
+++ EventManagementFPT/Pages/EventPage/Create.cshtml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -16,52 +14,68 @@ namespace EventManagementFPT.Pages.EventPage
 {
     public class CreateModel : PageModel
     {
-        private readonly EventManagementFPT.Model.EventManagementContext _context;
+        private readonly EventManagementContext _context;
         private readonly IEventService _eventService;
         private readonly IWebHostEnvironment _env;
 
-        public CreateModel(EventManagementFPT.Model.EventManagementContext context, IEventService eventService, IWebHostEnvironment env)
+        public CreateModel(EventManagementContext context, IEventService eventService,
+            IWebHostEnvironment env)
         {
             _context = context;
             _eventService = eventService;
-            _env = env; 
+            _env = env;
         }
 
         public IActionResult OnGet()
         {
-            ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
-            ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
-            TempData["success"] = "Page loaded!";
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+            if (role is "Admin" or "Host")
+            {
+                ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
+                ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
+                TempData["success"] = "Page loaded!";
 
-            return Page();
+                return Page();
+            }
+
+            return RedirectToPage("/Home");
         }
 
-        [BindProperty]
-        public Event Event { get; set; }
+        [BindProperty] public Event Event { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync(IFormFile customFile)
         {
-            if (!ModelState.IsValid)
+            var uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (uid == null) return RedirectToPage("/Authentication/Index");
+
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+            if (role is "Admin" or "Host")
             {
-                TempData["error"] = "Invalid data";
-                ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
-                ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
-                return Page();
+                if (!ModelState.IsValid)
+                {
+                    TempData["error"] = "Invalid data";
+                    ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
+                    ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
+                    return Page();
+                }
+
+                if (DateTime.Compare(Event.StartDate, Event.EndDate) >= 0)
+                {
+                    TempData["error"] = "Start date cannot be greater than end date";
+                    ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
+                    ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
+                    return Page();
+                }
+
+                TempData["success"] = "Add success";
+                string imageUrl = await UploadImage.UploadFile(customFile, _env);
+                Event.ImageUrl = imageUrl;
+                await _eventService.AddNewEvent(Event, uid);
+                return RedirectToPage("./Index");
             }
-            if (DateTime.Compare(Event.StartDate, Event.EndDate) >= 0)
-            {
-                TempData["error"] = "Start date cannot be greater than end date";
-                ViewData["Category"] = new SelectList(_context.Categories, "CategoryId", "Name");
-                ViewData["Venue"] = new SelectList(_context.Venues, "VenueId", "VenueName");
-                return Page();
-            }
-            TempData["success"] = "Add success";
-            string imageUrl = await UploadImage.UploadFile(customFile, _env);
-            Event.ImageUrl = imageUrl;
-            await _eventService.AddNewEvent(Event, User.FindFirst(ClaimTypes.NameIdentifier).Value);
-            return RedirectToPage("./Index");
-            //return RedirectToPage("./Index");
+
+            return RedirectToPage("/Home");
         }
     }
 }
