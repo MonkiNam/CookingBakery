235ace9a269c5d3f63d06b2906d1469cfd25f4a1
Nguyen Quang Dung <66056386+Wolf1910@users.noreply.github.com>
2022-07-12 16:53:02 +0700

    Update User Profile

--- EventManagementFPT/Pages/Home/UserProfile.cshtml.cs
+++ EventManagementFPT/Pages/Home/UserProfile.cshtml.cs
@@ -1,12 +1,38 @@
+using EventManagementFPT.Model;
+using EventManagementFPT.Modules.UserModule.Interface;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace EventManagementFPT.Pages.Home
 {
     public class UserProfileModel : PageModel
     {
+        private readonly IUserService _userService;
+
+        private readonly IWebHostEnvironment _env;
+
+        public UserProfileModel(IUserService userService, IWebHostEnvironment env)
+        {
+            _userService = userService;
+            _env = env;
+        }
+
+        [BindProperty]
+        public User user { get; set; }
+
         public void OnGet()
         {
+            user = _userService.GetUserByUserID(Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value));
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile customFile)
+        {
+            return Page();
         }
     }
 }
--- EventManagementFPT/wwwroot/js/site.js
+++ EventManagementFPT/wwwroot/js/site.js
@@ -15,11 +15,28 @@ if (filePreview != null) {
         var src = URL.createObjectURL(this.files[0]);
         var name = this.files[0].name;
         document.getElementById('customFileLabel').textContent = name;
-        previewImage.src = src;
+        previewImage.getAttribute('src') = src;
         document.getElementsByClassName('image-holder')[0].style.display = 'none';
     }
 }
 
+() => {
+    $('form > input').keyup(function () {
+
+        var empty = false;
+        $('form > input').each(function () {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#register').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#register').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
+}
 
 $(document).ready(function () {
     $('.toast').toast('show')
