e6ebe31850497ea3b15c3cd148d9526deaa371f5
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-12 15:47:00 +0700

    Add pagination for event

--- EventManagementFPT/Pages/Home/Index.cshtml.cs
+++ EventManagementFPT/Pages/Home/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using EventManagementFPT.Model;
 using EventManagementFPT.Modules.EventModule.Interface;
+using EventManagementFPT.Utils;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EventManagementFPT.Pages.Home
@@ -14,12 +16,13 @@ namespace EventManagementFPT.Pages.Home
             _eventService = eventService;
         }
 
-        public IEnumerable<Event> Event { get;set; }
+        public PaginatedList<Event> Event { get;set; }
         public IEnumerable<Event> NewestEvent { get;set; }
         
-        public void OnGet()
+        public void OnGetAsync(int? pageIndex)
         {
-            Event = _eventService.GetAll();
+            var events = _eventService.GetAll().Where(o => o.Status).AsQueryable();
+            Event = PaginatedList<Event>.Create(events, pageIndex ?? 1, 10);
             NewestEvent = _eventService.GetNewestEvents(3);
         }
     }
