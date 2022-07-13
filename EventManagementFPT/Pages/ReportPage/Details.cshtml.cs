8b2fc4a6959a3f17077efaf12f5afcac58bcdb20
Nguyen Quang Dung <66056386+Wolf1910@users.noreply.github.com>
2022-07-13 17:12:14 +0700

    [BE] Admin can now process report

--- EventManagementFPT/Pages/ReportPage/Details.cshtml.cs
+++ EventManagementFPT/Pages/ReportPage/Details.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using EventManagementFPT.Model;
 using EventManagementFPT.Modules.ReportModule.Interface;
 using Microsoft.AspNetCore.Authorization;
+using EventManagementFPT.Modules.UserModule.Interface;
 
 namespace EventManagementFPT.Pages.ReportPage
 {
@@ -15,10 +16,12 @@ namespace EventManagementFPT.Pages.ReportPage
     public class DetailsModel : PageModel
     {
         private readonly IReportService _reportService;
+        private readonly IUserService _userService;
 
-        public DetailsModel(IReportService reportService)
+        public DetailsModel(IReportService reportService, IUserService userService)
         {
             _reportService = reportService;
+            _userService = userService;
         }
 
         public Report Report { get; set; }
@@ -30,6 +33,7 @@ namespace EventManagementFPT.Pages.ReportPage
                 return NotFound();
             }
 
+            
             Report = await _reportService.GetReportByID(id);
 
             if (Report == null)
---
+++ EventManagementFPT/Pages/ReportPage/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventManagementFPT.Model;
+using EventManagementFPT.Modules.ReportModule.Interface;
+
+namespace EventManagementFPT.Pages.ReportPage
+{
+    public class EditModel : PageModel
+    {
+        private readonly IReportService _reportService;
+
+        public EditModel(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [BindProperty]
+        public Report Report { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id, int? status, string type)
+        {
+            if (type.Equals("edit"))
+            {
+                await _reportService.ChangeStatus(id, (int)status);
+                TempData["success"] = "Status of the report has been updated!";
+            }
+            else if (type.Equals("delete"))
+            {
+                await _reportService.DeleteReport(id);
+                TempData["success"] = "Report has been closed!";
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
